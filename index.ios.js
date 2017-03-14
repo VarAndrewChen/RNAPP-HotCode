6e013683721e6423184581741d65b21c7561db68
AndrewChen <varandrewchen@outlook.com>
2017-03-15 03:10:33 +0800

    setup

--- index.ios.js
+++ index.ios.js
@@ -4,50 +4,106 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    Navigator,
+    ListView
 } from 'react-native';
+import TabNavigator from 'react-native-tab-navigator';
+import Boy from './app/component/Boy';
+import ListViewTest from './app/component/ListViewTest';
+import FetchTest from './app/component/Fetch';
+
 
 export default class HotCode extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedTab: 'tb_popular'
+        };
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                {/* <TabNavigator>
+                    <TabNavigator.Item
+                        selected={this.state.selectedTab === 'tb_popular'}
+                        title="最热"
+                        selectedTitleStyle={{color:'red'}}
+                        renderIcon={() => <Image style={styles.icon} source={require('./res/images/ic_polular.png')}/>}
+                        renderSelectedIcon={() => <Image style={[styles.icon,{tintColor:'red'}]} source={require('./res/images/ic_polular.png')}/>}
+                        badgeText="1"
+                        onPress={() => this.setState({selectedTab: 'tb_popular'})}>
+                        <View style={styles.page_one}></View>
+                    </TabNavigator.Item>
+                    <TabNavigator.Item
+                        selected={this.state.selectedTab === 'tb_trending'}
+                        title="趋势"
+                        selectedTitleStyle={{color:'red'}}
+                        renderIcon={() => <Image style={styles.icon} source={require('./res/images/ic_trending.png')}/>}
+                        renderSelectedIcon={() => <Image style={[styles.icon,{tintColor:'red'}]} source={require('./res/images/ic_trending.png')}/>}
+                        onPress={() => this.setState({selectedTab: 'tb_trending'})}>
+                        <View style={styles.page_two}></View>
+                    </TabNavigator.Item>
+                    <TabNavigator.Item
+                        selected={this.state.selectedTab === 'tb_favorite'}
+                        title="收藏"
+                        selectedTitleStyle={{color:'red'}}
+                        renderIcon={() => <Image style={styles.icon} source={require('./res/images/ic_polular.png')}/>}
+                        renderSelectedIcon={() => <Image style={[styles.icon,{tintColor:'red'}]} source={require('./res/images/ic_polular.png')}/>}
+                        badgeText="1"
+                        onPress={() => this.setState({selectedTab: 'tb_favorite'})}>
+                        <View style={styles.page_one}></View>
+                    </TabNavigator.Item>
+                    <TabNavigator.Item
+                        selected={this.state.selectedTab === 'tb_mine'}
+                        title="我的"
+                        selectedTitleStyle={{color:'red'}}
+                        renderIcon={() => <Image style={styles.icon} source={require('./res/images/ic_trending.png')}/>}
+                        renderSelectedIcon={() => <Image style={[styles.icon,{tintColor:'red'}]} source={require('./res/images/ic_trending.png')}/>}
+                        onPress={() => this.setState({selectedTab: 'tb_mine'})}>
+                        <View style={styles.page_two}></View>
+                    </TabNavigator.Item>
+                </TabNavigator>
+                <Navigator
+                    initialRoute={{
+                        component: Boy
+                    }}
+                    renderScene={(route,navigator) => {
+                        let Component = route.component;
+                        return (<Component navigator={navigator} {...route.params}/>);
+                    }}>
+                </Navigator>
+                <ListViewTest/>*/}
+                <FetchTest/>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#F5FCFF'
+    },
+    page_one: {
+        flex: 1,
+        backgroundColor: 'red'
+    },
+    page_two: {
+        flex: 1,
+        backgroundColor: 'green'
+    },
+    icon: {
+        width: 22,
+        height: 22
+    }
 });
 
 AppRegistry.registerComponent('HotCode', () => HotCode);
